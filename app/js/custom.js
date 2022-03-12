8cddb500121b867d812f6c750fb7ece5b640d397
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-03-12 16:13:18 +0300

    added dark mode

---
+++ app/js/custom.js
@@ -0,0 +1,19 @@
+const Switch = document.getElementById("switch")
+Switch.addEventListener("change", function(e) {
+    let theme = e.currentTarget.checked
+    darkmode(theme)
+})
+darkmode(JSON.parse(localStorage.getItem("dark-theme")))
+
+function darkmode(theme) {
+    localStorage.setItem("dark-theme", theme)
+    if (theme === true) {
+
+        document.documentElement.setAttribute("data-theme", "dark")
+    } else if (theme === false) {
+
+
+        document.documentElement.setAttribute("data-theme", "light")
+    }
+    Switch.checked = theme;
+}
