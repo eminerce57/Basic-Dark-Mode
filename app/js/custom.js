604325d970baff376e0d1a0c8970a3e502832b81
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-07-01 01:56:57 +0300

    up

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,5 @@
 const Switch = document.getElementById("switch")
-Switch.addEventListener("change", function(e) {
+Switch.addEventListener("change", (e) => {
     let theme = e.currentTarget.checked
     darkmode(theme)
 })
@@ -8,11 +8,8 @@ darkmode(JSON.parse(localStorage.getItem("dark-theme")))
 function darkmode(theme) {
     localStorage.setItem("dark-theme", theme)
     if (theme === true) {
-
         document.documentElement.setAttribute("data-theme", "dark")
     } else if (theme === false) {
-
-
         document.documentElement.setAttribute("data-theme", "light")
     }
     Switch.checked = theme;
